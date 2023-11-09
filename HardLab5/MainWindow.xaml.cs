9dacf12c52f1cb26603317e1b1d6c17a74084d92
Elizaveta Pernikova <lisapernikowa@mail.ru>
2023-11-09 19:52:25 +0500

    Добавьте файлы проекта.

---
+++ HardLab5/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System;
+using System.Data;
+
+namespace HardLab5
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+    }
+
+}
